3e1c4d91a0c586abfe71aa93d07d809077a7b7c4
Nourgweda <37792672+Nourgweda@users.noreply.github.com>
2022-12-05 11:35:36 +0200

    Initial Commit

---
+++ SessionLiveActivity/SessionLiveActivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  SessionLiveActivityApp.swift
+//  SessionLiveActivity
+//
+//  Created by Nour Gweda on 05/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct SessionLiveActivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
